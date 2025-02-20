4371ddaf3041f7019d64e3eb9a1a18342585be59
jhgonzalezuruguay <102374190+jhgonzalezuruguay@users.noreply.github.com>
2025-02-20 14:13:12 -0300

    Update Health_Assitent.py

--- Health_Assitent.py
+++ Health_Assitent.py
@@ -109,8 +109,7 @@ if st.button("Diagnóstico"):
         # Display confidence message
         st.write(f"Confidence: {prediction_prob * 100:.2f}%")
         st.write(confidence_message)
-
-        else:
+else:
         st.warning("Selecciona un síntoma para su diagnóstico!")
 
 # Footer
